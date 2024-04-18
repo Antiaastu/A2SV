cc1babdc82806b1f31d606761787603baee4486e
Antiaastu <136442178+Antiaastu@users.noreply.github.com>
2024-04-18 12:37:59 +0300

    Insertion Sort

---
+++ Insertion Sort.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    value = arr[-1]
+    i = n - 2
+    while i >= 0 and arr[i] > value:
+        arr[i + 1] = arr[i]
+        i -= 1
+        print(*arr)
+    
+    arr[i + 1] = value
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
